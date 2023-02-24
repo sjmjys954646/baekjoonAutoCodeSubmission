947b6aac3847ce4247d838bd1680a58acd2260b9
sjmjys954646 <37840237+sjmjys954646@users.noreply.github.com>
2023-02-25 00:06:49 +0900

    [Silver III] Title: 눈덩이 굴리기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/21735. 눈덩이 굴리기/눈덩이 굴리기.cc
@@ -0,0 +1,91 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define rep1(i, n) for (int i = 1; i <= (int)(n); ++i)
+#define range(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+const int INF = 987654321;
+
+struct Point {
+	ll x, y;
+
+	bool operator < (const Point& a)
+	{
+		if (y == a.y)
+			return x < a.x;
+		else
+			return y < a.y;
+	}
+};
+
+ll ccw(const Point& a, const Point& b, const Point& c)
+{
+	return a.x * b.y + b.x * c.y + c.x * a.y - b.x * a.y - c.x * b.y - a.x * c.y;
+}
+
+ll dist(Point a, Point b)
+{
+	return (a.y - b.y) * (a.y - b.y) + (a.x - b.x) * (a.x - b.x);
+}
+
+
+int manhat(int startX, int startY, int endX, int endY)
+{
+	return abs(startX - endX) + abs(startY - endY);
+}
+
+int N, M;
+int ans;
+int tmp;
+vi v;
+
+void dfs(int cur,int time, int cost)
+{
+	cost += v[cur];
+
+	ans = max(ans, cost);
+	if (time == M)
+	{
+		ans = max(ans, cost);
+		return;
+	}
+
+	if(cur + 1 <= N)
+		dfs(cur + 1, time + 1, cost);
+	if(cur + 2 <= N )
+		dfs(cur + 2, time + 1, cost/2);
+}
+
+int main()
+{
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+	cout.tie(0);
+
+	cin >> N >> M;
+	v.push_back(0);
+	rep(i, N)
+	{
+		cin >> tmp;
+		v.push_back(tmp);
+	}
+
+	dfs(0,0,1);
+
+	cout << ans;
+
+	return 0;
+}
