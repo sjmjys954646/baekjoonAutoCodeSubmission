4a26c0060a2c8bb73b750bd6cb88973a362b4a30
sjmjys954646 <37840237+sjmjys954646@users.noreply.github.com>
2023-03-08 11:16:57 +0900

    [Gold IV] Title: 흩날리는 시험지 속에서 내 평점이 느껴진거야, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Gold/17951. 흩날리는 시험지 속에서 내 평점이 느껴진거야/흩날리는 시험지 속에서 내 평점이 느껴진거야.cc
@@ -0,0 +1,105 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define rep1(i, n) for (int i = 1; i <= (int)(n); ++i)
+#define range(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+const int INF = 987654321;
+
+struct Point {
+	ll x, y;
+
+	bool operator < (const Point& a)
+	{
+		if (y == a.y)
+			return x < a.x;
+		else
+			return y < a.y;
+	}
+};
+
+ll ccw(const Point& a, const Point& b, const Point& c)
+{
+	return a.x * b.y + b.x * c.y + c.x * a.y - b.x * a.y - c.x * b.y - a.x * c.y;
+}
+
+ll dist(Point a, Point b)
+{
+	return (a.y - b.y) * (a.y - b.y) + (a.x - b.x) * (a.x - b.x);
+}
+
+
+int manhat(int startX, int startY, int endX, int endY)
+{
+	return abs(startX - endX) + abs(startY - endY);
+}
+
+int arr[100002];
+int N, K;
+
+int check(int mid)
+{
+	int sum = 0;
+	int cnt = 0;
+	for (int i = 0; i < N; i++)
+	{
+		sum += arr[i];
+		if (sum >= mid)
+		{
+			sum = 0;
+			cnt++;
+		}
+	}
+
+	if (cnt >= K)
+		return 1;
+	else
+		return 0;
+}
+
+int main()
+{
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+	cout.tie(0);
+
+	cin >> N >> K;
+
+	int lo = 0;
+	int hi = 1;
+
+	rep(i, N)
+	{
+		cin >> arr[i];
+		hi += arr[i];
+	}
+
+	while (lo + 1 < hi)
+	{
+		int mid = (lo + hi) / 2;
+		if (check(mid))
+		{
+			lo = mid;
+		}
+		else
+		{
+			hi = mid;
+		}
+	}
+	cout << lo ;
+
+	return 0;
+}
