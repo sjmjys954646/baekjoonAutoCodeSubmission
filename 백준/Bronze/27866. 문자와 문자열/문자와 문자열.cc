83e5575f2b5df9f264c77a583800483c8b982106
sjmjys954646 <37840237+sjmjys954646@users.noreply.github.com>
2023-03-09 14:26:27 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+#define rep(i, n) for (int i = 0; i < (int)(n); ++i)
+#define rep1(i, n) for (int i = 1; i <= (int)(n); ++i)
+#define range(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define F first
+#define S second
+
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+const int INF = 987654321;
+
+struct Point {
+	ll x, y;
+
+	bool operator < (const Point& a)
+	{
+		if (y == a.y)
+			return x < a.x;
+		else
+			return y < a.y;
+	}
+};
+
+ll ccw(const Point& a, const Point& b, const Point& c)
+{
+	return a.x * b.y + b.x * c.y + c.x * a.y - b.x * a.y - c.x * b.y - a.x * c.y;
+}
+
+ll dist(Point a, Point b)
+{
+	return (a.y - b.y) * (a.y - b.y) + (a.x - b.x) * (a.x - b.x);
+}
+
+
+int manhat(int startX, int startY, int endX, int endY)
+{
+	return abs(startX - endX) + abs(startY - endY);
+}
+
+int main()
+{
+	cin.tie(0);
+	ios_base::sync_with_stdio(false);
+	cout.tie(0);
+
+	string tmp;
+	int N;
+	cin >> tmp;
+	cin >> N;
+
+	cout << tmp[N - 1];
+;
+
+	return 0;
+}
